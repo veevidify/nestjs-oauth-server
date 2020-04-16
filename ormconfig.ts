c9e36e30256b480df7e764fd7085bdd1e69c6d97
V Ng <vee.vidify@gmail.com>
2020-04-16 22:45:43 +1000

    refactor: configs

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,11 +2,11 @@ import { ConnectionOptions } from 'typeorm';
 
 const typeormConfig: ConnectionOptions = {
   type: 'postgres',
-  database: process.env.POSTGRES_USER || 'dev',
+  database: process.env.POSTGRES_DB || 'dev',
   username: process.env.POSTGRES_USER || 'dev',
-  password: process.env.POSTGRES_USER || 'dev',
-  host: 'postgres',
-  port: 5432,
+  password: process.env.POSTGRES_PASS || 'dev',
+  host: process.env.POSTGRES_HOST || 'postgres',
+  port: process.env.POSTGRES_PORT || 5432,
   synchronize: false,
   entities: [__dirname + '/src/entities/*.entity.ts'],
   migrations: ['src/migrations/*.migration.ts'],
